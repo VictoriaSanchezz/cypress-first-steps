12cd667ecf1f3875f663677c5541dbedaa78700f
VictoriaSanchezz <victoriasanchezzr9@gmail.com>
2025-02-15 16:01:59 -0300

    Page objects added

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -1,15 +1,19 @@
 import userData from '../fixtures/userData.json'
+import LoginPage from '../pages/loginPage.js'
+import DashboardPage from '../pages/dashboardPage.js'
+import MenuPage from '../pages/menuPage.js'
+
+const loginPage = new LoginPage()
+const dashboardPage = new DashboardPage()
+const menuPage = new MenuPage()
+
 
 describe('Orange HRM Tests', () => {
 
   const selectorsList = {
-    usernameField: "[name='username']",
-    passwordField: "[name='password']",
-    loginButton: "[type='submit']",
-    sectionTitleTopBar: '.oxd-topbar .oxd-topbar-header-breadcrumb-module',
-    dashboardGrid: ".orangehrm-dashboard-grid",
-    wrongCredentialAlert: '.oxd-alert',
-    myInfoButton: "[href='/web/index.php/pim/viewMyDetails']",
+    
+    
+    
     firstNameField: "[name='firstName']",
     middleNameField: "[name='middleName']",
     lastNameField: "[name='lastName']",
@@ -17,32 +21,46 @@ describe('Orange HRM Tests', () => {
     dateField: "[placeholder='yyyy-dd-mm']",
     dateCloseButton: ".--close",
     submitButton: "[type='submit']",
-    genderButton: ".oxd-radio-wrapper"
+    genderButton: ".oxd-radio-wrapper",
+    comboboxSelector: ".oxd-select-text--arrow",
+    firstComboboxField: ":nth-child(173)",
+    secondComboboxField: ".oxd-select-dropdown > :nth-child(3)",
+    thirdComboboxField: ".oxd-select-dropdown > :nth-child(6)"
+    
 
   }
 
   it.only('User Info Update - Success', () => {
-    cy.visit('/auth/login')
-    cy.get(selectorsList.usernameField).type(userData.userSuccess.username)
-    cy.get(selectorsList.passwordField).type(userData.userSuccess.password)
-    cy.get(selectorsList.loginButton).click()
-    cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
-    cy.get(selectorsList.dashboardGrid)   
-    cy.get(selectorsList.myInfoButton).click()
-    cy.get(selectorsList.firstNameField).clear().type('John')
-    cy.get(selectorsList.middleNameField).clear().type('John')
-    cy.get(selectorsList.lastNameField).clear().type('John')
-    cy.get(selectorsList.genericField).eq(3).clear().type('12345')
-    cy.get(selectorsList.genericField).eq(4).clear().type('54321')
-    cy.get(selectorsList.genericField).eq(5).clear().type('1122334455')
-    cy.get(selectorsList.dateField).eq(0).clear().type('2025-03-10')
-    cy.get(selectorsList.dateCloseButton).click()
-    cy.get(selectorsList.dateField).eq(1).clear().type('2000-02-10')
-    cy.get(selectorsList.dateCloseButton).click()
-    cy.get(selectorsList.genderButton).eq(0).click()
-    cy.get(selectorsList.submitButton).eq(0).click()
-    cy.get('body').should('contain', 'Successfully Updated')
+    loginPage.accessLoginPage()
+    loginPage.loginWithAnyUser(userData.userSuccess.username, userData.userSuccess.password)
+    dashboardPage.dashboardPageCheck()
+    menuPage.accessMyInfo()
+       
     
+    //cy.get(selectorsList.firstNameField).clear().type('John')
+    //cy.get(selectorsList.middleNameField).clear().type('John')
+    //cy.get(selectorsList.lastNameField).clear().type('John')
+    //cy.get(selectorsList.genericField).eq(3).clear().type('12345')
+    //cy.get(selectorsList.genericField).eq(4).clear().type('54321')
+    //cy.get(selectorsList.genericField).eq(5).clear().type('1122334455')
+    //cy.get(selectorsList.dateField).eq(0).clear().type('2025-03-10')
+    //cy.get(selectorsList.dateCloseButton).click()
+    //cy.get(selectorsList.comboboxSelector).eq(0).click()
+    //cy.get(selectorsList.firstComboboxField).click()
+    //cy.get(selectorsList.comboboxSelector).eq(1).click()
+    //cy.get(selectorsList.secondComboboxField).click()
+    //cy.get(selectorsList.dateField).eq(1).clear().type('2000-02-10')
+    //cy.get(selectorsList.dateCloseButton).click()
+    //cy.get(selectorsList.genericField).eq(8).clear().type('123123')
+    //cy.get(selectorsList.genderButton).eq(0).click()
+    //cy.get(selectorsList.submitButton).eq(0).click({force: true})
+    //cy.get('body').should('contain', 'Successfully Updated')
+    //cy.get(selectorsList.comboboxSelector).eq(2).click()
+    //cy.get(selectorsList.thirdComboboxField).click()
+    //cy.get(selectorsList.submitButton).eq(1).click({force: true})
+    //cy.get('body').should('contain', 'Successfully Saved')
+    
+
   })
 
   it('Login - Fail', () => {
